4cbc8505bec1cf64d9a2e9e0a0ac106d5840b385
Victor <victorhvercosag@gmail.com>
2025-08-11 19:36:49 -0300

    revisando estrutura de decisão em C#

---
+++ Revisao/Program.cs
@@ -0,0 +1,17 @@
+﻿
+// Estrutura de decisão
+int idade = 23;
+if (idade >= 18) 
+    Console.WriteLine("Maior idade");
+else {
+    Console.WriteLine("Menor idade");
+}
+
+
+
+
+/*Console.Write("Digite o ano de nascimento: ");
+int ano = Convert.ToInt32(Console.ReadLine());
+int idade = 2025 - ano;
+Console.Write("Sua idade é " + idade);*/
+
